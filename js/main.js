151b3f701527933089b00edd1eef3d7ed9266e96
Tommy Leng <tommyleng@yahoo.com>
2017-06-28 13:00:32 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	lightbox.option({
+		'alwaysShowNavOnTouchDevices': true,
+		'wrapAround': true
+	});
+	$("#searchFor").keyup(function(){
+        // Retrieve the input field text 
+       	var filter = $(this).val();
+		$(".responsive").each(function(){
+		    // // If the div item does not contain the text phrase fade it out
+		    if($(this).find('img').attr('alt').search(new RegExp(filter, "i")) < 0) {
+		    	$(this).fadeOut(500);
+		    	console.log($(this).find('img').attr('alt').search(new RegExp(filter, "i")) < 0);
+		    // Show the div item if the phrase matches 
+		    } else {
+		        $(this).show();
+		    }
+		});
+	})
+})
